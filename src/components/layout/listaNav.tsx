3b78a9cbd5060b51b2b1931d7d8c304daae2d3ef
Nicky <nicolasyanase18@gmail.com>
2025-01-17 19:23:35 -0300

    feat: border dinamica e scroll

--- src/components/layout/listaNav.tsx
+++ src/components/layout/listaNav.tsx
@@ -7,19 +7,19 @@ export interface listaNavProps{
 export default function ListaNav(props: listaNavProps) {
   return(
     <ul className={`flex gap-8 md:gap-5 text-zinc-400 ${props.className}`}>
-      <li className="transition-all duration-300 hover:text-white">
+      <li className="transition-all duration-300 hover:text-white p-1">
         <Link href={"/"} className="flex gap-2"><IconHome className="md:hidden"/> Home</Link>
       </li>
-      <li className="transition all durantion-500 hover:text-white">
+      <li className="transition all durantion-500 hover:text-white p-1">
         <Link href={"/"} className="flex gap-2 "><IconCube className="md:hidden"/>Projetos</Link>
       </li>
-      <li className="transition all durantion-500 hover:text-white">
+      <li className="transition all durantion-500 hover:text-white p-1">
         <Link href={"/"} className="flex gap-2 "><IconUser className="md:hidden"/>Sobre</Link>
       </li>
-      <li className="transition all durantion-500 hover:text-white">
+      <li className="transition all durantion-500 hover:text-white p-1">
         <Link href={"/"}className="flex gap-2 "><IconCode className="md:hidden"/>Skills</Link>
       </li>
-      <li className="transition all durantion-500 hover:text-white">
+      <li className="transition all durantion-500 hover:text-white p-1">
         <Link href={"/"}className="flex gap-2 "><IconMedal className="md:hidden"/>Resumo</Link>
       </li>
     </ul>
