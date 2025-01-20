299b3643634d1e487e3cce057b9fa95fbe845749
Nicolas Yanase <142536049+Nick-Yanase@users.noreply.github.com>
2025-01-20 11:26:22 -0300

    feat:add preload

--- src/components/layout/template.tsx
+++ src/components/layout/template.tsx
@@ -1,5 +1,8 @@
+'use client';
+import { useEffect, useState } from "react";
 import Footer from "./footer";
 import Header from "./header";
+import PreloaderAnime from "../preloaderAnime";
 
 export interface templateProps {
   children: React.ReactNode;
@@ -7,17 +10,48 @@ export interface templateProps {
 }
 
 export default function Template(props: templateProps) {
+  const [loading, setLoading] = useState(true);
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Oculta o Preloader
+      setTimeout(() => setShowContent(true), 500); // Mostra o conteúdo após a transição
+    }, 1800); // Simula um carregamento de 2 segundos
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="flex flex-col">
-      <Header />
-      <main
-        className={`w-full flex-col flex justify-center items-center bg-zinc-950 overflow-hidden${
-          props.className ?? ``
-        }`}
-      >
-        {props.children}
-      </main>
-      <Footer />
+    <div
+      className={`w-full h-screen bg-[#120B24] transition-all duration-500`}
+    >
+      {/* Preloader */}
+      {loading && (
+        <div
+          className={`w-full h-full absolute inset-0 transition-opacity duration-200 ${
+            showContent ? 'opacity-0' : 'opacity-100'
+          }`}
+        >
+          <PreloaderAnime />
+        </div>
+      )}
+
+      {/* Conteúdo principal */}
+      {!loading && (
+        <div
+          className={`flex flex-col transition-opacity duration-500 ${
+            showContent ? 'opacity-100' : 'opacity-0'
+          }`}
+        >
+          <Header />
+          <main
+            className={`w-full flex-col flex justify-center items-center bg-zinc-950 overflow-hidden ${props.className ?? ''}`}
+          >
+            {props.children}
+          </main>
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/preloaderAnime.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Logotipo from "./layout/logotipo";
+
+export default function PreloaderAnime(){
+  return(
+    <div className="w-full h-screen inset-0 flex gap-4 flex-col items-center justify-center" >
+      {/* <Image src={"/image/NY-LOGOTIPO.SVG"} alt="LOGOTIPO" width={300}height={300}/> */}
+      <div className="size-20 relative">
+        <Image src={"../../images/NY-LOGOTIPO.svg"} alt="logotipo" fill/>
+      </div>
+      <Image
+        src="/lotties/preloader.gif" // Certifique-se de usar um formato compatível
+        width={150} 
+        height={150}
+        alt="carregando ..."
+        unoptimized // Necessário para GIFs animados
+      />
+    </div>
+  )
+}
