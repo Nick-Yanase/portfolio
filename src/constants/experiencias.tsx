1ee198ab66ce2d1c3aaa57e3caf13ab90a69568b
Nicolas Yanase <nicolasyanase18@gmail.com>
2025-08-20 09:23:35 -0300

    chore(skills): atualiza img de js

--- src/constants/experiencias.tsx
+++ src/constants/experiencias.tsx
@@ -1,47 +1,45 @@
-import Experiencia from "../models/experiencia"
+import Experiencia from "../models/experiencia";
 
 const experiencias: Experiencia[] = [
- {
-  id:2,
-  data: "08/2025 - Atualmente",
-  titulo: "Estágio em dev",
-  organizacao: "Yoodash",
-  iconOrg: "/images/career/experience/yoodash.png",
-  skills:[
-    "Desenvolvimento de Mobile", 
-    "Automação de Web",
-    "Versionamento",
-    "Metodologia Agil"
-  ]
+  {
+    id: 2,
+    data: "08/2025 - Atualmente",
+    titulo: "Estágio em dev",
+    organizacao: "Yoodash",
+    iconOrg: "/images/career/experience/yoodash.png",
+    skills: [
+      "Desenvolvimento de Mobile",
+      "Automação de Web",
+      "Versionamento",
+      "Metodologia Agil",
+    ],
+  },
+  {
+    id: 1,
+    data: "08/2024 - 08/2025",
+    titulo: "Estágio em TI",
+    organizacao: "hospital sagrada família",
+    iconOrg: "/images/career/experience/HSF.png",
+    skills: [
+      "Desenvolvimento de softwares",
+      "Automação de Recursos",
+      "Gestão de Servidores",
+      "Análises de sistemas",
+    ],
+  },
+  {
+    id: 2,
+    data: "04/2022 - 07/2024",
+    titulo: "Designer / dev",
+    organizacao: "freelancer",
+    iconOrg: "/images/career/experience/DEV-FREE.png",
+    skills: [
+      "Desenvolvimento de softwares",
+      "Design responsivos",
+      "Landing Pages",
+      "Criações de logotipos",
+    ],
+  },
+];
 
- },
- {
-  id:1,
-  data: "08/2024 - 08/2025",
-  titulo: "Estágio em TI",
-  organizacao: "hospital sagrada família",
-  iconOrg: "/images/career/experience/HSF.png",
-  skills:[
-    "Desenvolvimento de softwares", 
-    "Automação de Recursos",
-    "Gestão de Servidores",
-    "Análises de sistemas"
-  ]
-
- },
- {
-  id:2,
-  data: "04/2022 - 07/2024",
-  titulo: "Designer / dev",
-  organizacao: "freelancer",
-  iconOrg: "/images/career/experience/DEV-FREE.png",
-  skills:[
-    "Desenvolvimento de softwares", 
-    "Design responsivos",
-    "Landing Pages",
-    "Criações de logotipos"
-  ]
- },
-]
-
-export default experiencias
+export default experiencias;
